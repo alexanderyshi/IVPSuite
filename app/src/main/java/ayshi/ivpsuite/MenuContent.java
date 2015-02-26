3102836c59c4163f4a96dcb8440a487d521a3f04
Alex Shi <alexander_shi@hotmail.com>
2015-02-26 07:14:47 -0500

    first commit

---
+++ app/src/main/java/ayshi/ivpsuite/MenuContent.java
@@ -0,0 +1,49 @@
+package ayshi.ivpsuite;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for user interface in IVP Suite app
+ */
+public class MenuContent {
+    public static List<MenuItem> ITEMS = new ArrayList<MenuItem>();
+
+    /**
+     * A map of items, by ID.
+     */
+    public static Map<String, MenuItem> ITEM_MAP = new HashMap<String, MenuItem>();
+
+    static {
+        // Add 3 sample items.
+        addItem(new MenuItem("new_source", "New Source"));
+        addItem(new MenuItem("threshold", "Threshold"));
+        addItem(new MenuItem("gamma_shift", "Gamma Shift"));
+        addItem(new MenuItem("histogram_transform", "Histogram Transform"));
+    }
+
+    private static void addItem(MenuItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class MenuItem {
+        public String id;
+        public String content;
+
+        public MenuItem(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
